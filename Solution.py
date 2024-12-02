35271a1d330ff5b0bafd2d937650cbc185f3c169
JChen5543 <dozensphinx@gmail.com>
2024-12-02 12:04:16 -0500

    solution is now correct

--- Solution.py
+++ Solution.py
@@ -1,6 +1,18 @@
-def countRepeat(a, b):
-    for x in (len(a) - 1):
-        if a[x + 1] == b:
-            count += 1
+def countRepeat(a: str, b: str) -> bool:
+    count = 0
+    # Variable used to store if the previous element was b
+    previous_is_b = False
+    # Variable used to prevent overcounting a series of repetitions
+    repeat_lock = False
+    for i in a:
+        if i == b:
+            if previous_is_b == True and repeat_lock == False:
+                count += 1
+                # Once a repetition is counted, we have to prevent this from running in order to avoid overcounting
+                repeat_lock = True
+            previous_is_b = True
+        else:
+            repeat_lock = False
+            previous_is_b = False
             
     return count
